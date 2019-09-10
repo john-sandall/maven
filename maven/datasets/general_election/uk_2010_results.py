fc9cb8d503cc6bd794f5ce479894cc96b9f9a20e
George Lenton <george@coefficient.ai>
2019-09-10 13:44:08 +0100

    Altered uk_2010_results, formatting and structure
    * moved variables into `__init__`
    * additional context to some comments

--- maven/datasets/general_election/uk_2010_results.py
+++ maven/datasets/general_election/uk_2010_results.py
@@ -22,6 +22,7 @@ class UK2010Results:
 
     def __init__(self, directory=Path("data/general-election/UK/2010/results")):
         self.directory = Path(directory)
+
         self.sources = [
             (
                 "http://www.electoralcommission.org.uk/__data/assets/excel_doc/0003/105726/",
@@ -32,67 +33,31 @@ class UK2010Results:
                 "GE2010-results-flatfile-website.xls",
             ),
         ]
+        self.raw_data_dir = self.directory / "raw"
 
-    def retrieve(self):
-        """Retrieve results data for the United Kingdom's 2010 General Election."""
-        target = self.directory / "raw"
-        os.makedirs(target, exist_ok=True)  # create directory if it doesn't exist
-        for url, filename in self.sources:
-            response = requests.get(url + filename)
-            if response.status_code == 200:
-                with open(target / filename, "wb") as f:
-                    f.write(response.content)
-                print(f"Successfully downloaded raw data into {target.resolve()}")
-                return
-            warnings.warn(
-                f"Received status 404 when trying to retrieve {url}{filename}"
-            )
-        raise RuntimeError("Unable to download UK 2010 General Election results data.")
-
-    def process(self):
-        """Process results data for the United Kingdom's 2010 General Election."""
-        processed_results_filename = "general_election-uk-2010-results.csv"
-        processed_results_full_filename = "general_election-uk-2010-results-full.csv"
-        processed_results_location = (
-            self.directory / "processed" / processed_results_filename
+        # create variables for storing the procesed results
+        self.processed_results_filename = "general_election-uk-2010-results.csv"
+        self.processed_results_full_filename = (
+            "general_election-uk-2010-results-full.csv"
         )
-        processed_results_full_location = (
-            self.directory / "processed" / processed_results_full_filename
+        self.processed_results_location = (
+            self.directory / "processed" / self.processed_results_filename
         )
-        os.makedirs(
-            self.directory / "processed", exist_ok=True
-        )  # create directory if it doesn't exist
-
-        # TODO: Refactor these sections into functions to make it easier to read.
-
-        ##########################
-        # GENERAL ELECTION RESULTS
-        ##########################
-        print("Read and clean GE2010-results-flatfile-website.xls")
-
-        # Import general election results
-        results = pd.read_excel(
-            self.directory / "raw" / "GE2010-results-flatfile-website.xls",
-            sheet_name="Party vote share",
+        self.processed_results_full_location = (
+            self.directory / "processed" / self.processed_results_full_filename
         )
 
-        # Remove rows where Constituency Name is blank
-        blank_rows = results["Constituency Name"].isnull()
-        results = results[-blank_rows].copy()
+        # will ensure that these dimensions are met
+        self.expected_row_dim = 650
+        self.expected_col_dim = 144
 
-        # Set NA vals to zero
-        for col in results.columns[6:]:
-            results[col] = results[col].fillna(0)
-        assert results.shape == (650, 144)
+        # 6 here is tehe column which contains vote counts, and this column
+        # (col 6, indexing from 0) is the total for each vote.
+        self.first_col_with_party_vote_info = 6
 
-        # Save this for convenience
-        results_full = results.copy()
-
-        ############################
-        # ADDITIONAL TRANSFORMATIONS
-        ############################
-        # Filter to metadata cols + parties of interest
-        parties_lookup = {
+        # Filter to metadata cols + parties of interest. The original data has
+        # ~139 parties, we want to filter this down for future analysis.
+        self.parties_lookup = {
             "Con": "con",
             "Lab": "lab",
             "LD": "ld",
@@ -109,72 +74,152 @@ class UK2010Results:
             # Other
             "Other": "other",
         }
-        other_parties = list(
-            set(results.columns) - set(results.columns[:6]) - set(parties_lookup.keys())
+
+        self.uk_regions_to_NI_scotland_london_and_not_london = {
+            "East Midlands": "England_not_london",
+            "Eastern": "England_not_london",
+            "London": "London",
+            "North East": "England_not_london",
+            "North West": "England_not_london",
+            "Northern Ireland": "NI",
+            "Scotland": "Scotland",
+            "South East": "England_not_london",
+            "South West": "England_not_london",
+            "Wales": "Wales",
+            "West Midlands": "England_not_london",
+            "Yorkshire and the Humber": "England_not_london",
+        }
+
+    def retrieve(self):
+        """Retrieve results data for the United Kingdom's 2010 General Election."""
+        os.makedirs(
+            self.raw_data_dir, exist_ok=True
+        )  # create directory if it doesn't exist
+        for url, filename in self.sources:
+            response = requests.get(url + filename)
+            if response.status_code == 200:
+                with open(self.raw_data_dir / filename, "wb") as f:
+                    f.write(response.content)
+                print(
+                    f"Successfully downloaded raw data into {self.raw_data_dir.resolve()}"
+                )
+                return
+            warnings.warn(
+                # TODO: why doesn't f string formatting work here?
+                "Received status 404 when trying to retrieve {}{}".format(url, filename)
+            )
+        raise RuntimeError("Unable to download UK 2010 General Election results data.")
+
+    def process(self):
+        """Process results data for the United Kingdom's 2010 General Election."""
+        os.makedirs(
+            self.directory / "processed", exist_ok=True
+        )  # create directory if it doesn't exist
+
+        #######################################################################
+
+        # TODO: Refactor these sections into functions to make it easier to read.
+
+        ##########################
+        # GENERAL ELECTION RESULTS
+        ##########################
+        print(f"Read and clean {self.sources[0][1]}")
+
+        # Import general election results from the correct sheet, there are two
+        # sheets, the other being 'Party Abbreviations'
+        results = pd.read_excel(
+            self.directory / "raw" / self.sources[0][1], sheet_name="Party vote share"
         )
+
+        # Remove rows where Constituency Name is blank - in the spreadsheet
+        # this is only one row, the last row of the sheet
+        blank_rows = results["Constituency Name"].isnull()
+        results = results[-blank_rows].copy()
+
+        # NA represents zero votes for that party within that consituency, so
+        # set them to zero.
+        results.to_csv("d2.csv")
+        for party_vote_result in results.columns[self.first_col_with_party_vote_info :]:
+            results[party_vote_result] = results[party_vote_result].fillna(0)
+
+        # missing rows/columns will impact further analysis
+        assert results.shape == (self.expected_row_dim, self.expected_col_dim), (
+            f"Dimensions of data are incorrect, expect {self.expected_row_dim} rows"
+            f"and {self.expected_col_dim} columns. "
+            f"data has {results.shape[0]} rows, {results.shape[1]} cols"
+        )
+
+        # Save this for convenience
+        results_full = results.copy()
+        results_full.to_csv("./test-full-results.csv")
+
+        ############################
+        # ADDITIONAL TRANSFORMATIONS
+        ############################
+
+        other_parties = list(
+            set(results.columns)
+            - set(results.columns[: self.first_col_with_party_vote_info])
+            - set(self.parties_lookup.keys())
+        )
+
         results["Other"] = results.loc[:, other_parties].sum(axis=1)
         results = results.loc[
-            :, list(results.columns[:6]) + list(parties_lookup.keys())
+            :, list(results.columns[:6]) + list(self.parties_lookup.keys())
         ]
 
-        # Rename parties
+        # Rename parties using the defined values in parties_lookup, ignore
+        # columns relating to Press Association Reference, Constituency Name,
+        # Region, Election Year, and Electorate.
+        #
+        # TODO seems as though this could be done using .lower() and .title()
         results.columns = [
-            parties_lookup[x] if x in parties_lookup else x for x in results.columns
+            self.parties_lookup[x] if x in self.parties_lookup else x
+            for x in results.columns
         ]
 
-        # Calculate constituency level vote share
-        for party in parties_lookup.values():
+        # Calculate constituency level vote share proportion (pc=proportion count)
+        for party in self.parties_lookup.values():
             results[party + "_pc"] = results[party] / results["Votes"]
 
         # Create PANO -> geo lookup
+        # Store regions in England as either London or not-London, rather than
+        # having East/West Midlands and such.
         results["geo"] = results.Region.map(
-            {
-                "East Midlands": "England_not_london",
-                "Eastern": "England_not_london",
-                "London": "London",
-                "North East": "England_not_london",
-                "North West": "England_not_london",
-                "Northern Ireland": "NI",
-                "Scotland": "Scotland",
-                "South East": "England_not_london",
-                "South West": "England_not_london",
-                "Wales": "Wales",
-                "West Midlands": "England_not_london",
-                "Yorkshire and the Humber": "England_not_london",
-            }
+            self.uk_regions_to_NI_scotland_london_and_not_london
         )
-        assert results.loc[237.0, "geo"] == "London"
 
-        # Calculate geo-level vote share
-        # TODO: Do we use this?
-        results_by_geo = (
-            results.loc[:, ["Votes", "geo"] + list(parties_lookup.values())]
-            .groupby("geo")
-            .sum()
-        )
-        results_by_geo_voteshare = results_by_geo.div(results_by_geo["Votes"], axis=0)
-        del results_by_geo_voteshare["Votes"]
+        # TODO - (is the choice of 237 arbitary)
+        assert results.loc[237.0, "geo"] == "London"
 
         # Who won?
         def winner(row):
-            all_parties = set(results_full.columns[6:]) - set(["Other"])
-            winning_party = row[all_parties].sort_values(ascending=False).index[0]
-            if winning_party in parties_lookup.keys():
-                winning_party = parties_lookup[winning_party]
+            """
+            given a row representing outome for a consituency sort and return
+            the winning party 
+            """
+            winning_party = (
+                row[self.first_col_with_party_vote_info :]
+                .sort_values(ascending=False)
+                .index[0]
+            )
+            if winning_party in self.parties_lookup.keys():
+                winning_party = self.parties_lookup[winning_party]
             elif winning_party == "Speaker":
                 winning_party = "other"
-            return winning_party
+            return winning_party.lower()
 
+        results_full.to_csv("dfull.csv")
         results["winner"] = results_full.apply(winner, axis=1)
-        # Check Labour won 306 seats in 2010.
+        results.to_csv("d1.csv")
+        # Check Conservative won 306 seats in 2010.
         assert (
-            results.groupby("winner")
-            .count()["Constituency Name"]
-            .sort_values(ascending=False)[0]
-            == 306
-        )
+            results.winner.value_counts()[0] == 306
+        ), "Conservative should have won 306 seats in 2010, data shows otherwise"
 
         # EXPORT
-        print(f"Exporting dataset to {processed_results_location.resolve()}")
-        results.to_csv(processed_results_location, index=False)
-        results_full.to_csv(processed_results_full_location, index=False)
+        print(f"Exporting dataset to {self.processed_results_location.resolve()}")
+        results.to_csv(self.processed_results_location, index=False)
+        print(f"Exporting dataset to {self.processed_results_full_location.resolve()}")
+        results_full.to_csv(self.processed_results_full_location, index=False)
+
