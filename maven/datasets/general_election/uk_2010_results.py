a348ecadced15efed232618b1808b437e20d4084
John Sandall <john@coefficient.ai>
2020-01-26 07:53:01 +0000

    Rewrote and simplified general-election/UK/2010/results to use HoC data

--- maven/datasets/general_election/uk_2010_results.py
+++ maven/datasets/general_election/uk_2010_results.py
@@ -2,6 +2,9 @@
 Results data for the United Kingdom's 2010 General Election.
 
 Sources:
+    - http://researchbriefings.files.parliament.uk/documents/CBP-8647/1918-2017election_results.csv
+
+Deprecated sources:
     - http://www.electoralcommission.org.uk/__data/assets/excel_doc/0003/105726/GE2010-results-flatfile-website.xls
     - https://s3-eu-west-1.amazonaws.com/sixfifty/GE2010-results-flatfile-website.xls
 
@@ -16,6 +19,8 @@ from pathlib import Path
 import pandas as pd
 import requests
 
+from maven import utils
+
 
 class UK2010Results:
     """Handles results data for the United Kingdom's 2010 General Election."""
@@ -23,7 +28,10 @@ class UK2010Results:
     def __init__(self, directory=Path("data/general-election/UK/2010/results")):
         self.directory = Path(directory)
         self.sources = [
-            ("https://s3-eu-west-1.amazonaws.com/sixfifty/", "GE2010-results-flatfile-website.xls",),
+            (
+                "http://researchbriefings.files.parliament.uk/documents/CBP-8647/",
+                "1918-2017election_results_by_pcon.xlsx",
+            ),
         ]
 
     def retrieve(self):
@@ -37,109 +45,69 @@ class UK2010Results:
                     file.write(response.content)
                 print(f"Successfully downloaded raw data into {target.resolve()}")
                 return
-            warnings.warn(f"Received status 404 when trying to retrieve {url}{filename}")
+            else:
+                warnings.warn(f"Received status 404 when trying to retrieve {url}{filename}")
         raise RuntimeError("Unable to download UK 2010 General Election results data.")
 
     def process(self):
         """Process results data for the United Kingdom's 2010 General Election."""
         filename = self.sources[0][1]
         processed_results_filename = "general_election-uk-2010-results.csv"
-        processed_results_full_filename = "general_election-uk-2010-results-full.csv"
         processed_results_location = self.directory / "processed" / processed_results_filename
-        processed_results_full_location = self.directory / "processed" / processed_results_full_filename
         os.makedirs(self.directory / "processed", exist_ok=True)  # create directory if it doesn't exist
 
-        ##########################
-        # GENERAL ELECTION RESULTS
-        ##########################
         print(f"Read and clean {filename}")
 
         # Import general election results
-        results = pd.read_excel(self.directory / "raw" / filename, sheet_name="Party vote share")
-
-        # Remove rows where Constituency Name is blank (one row only - the last row of the sheet)
-        blank_rows = results["Constituency Name"].isnull()
-        results = results[-blank_rows].copy()
-
-        # Set NA vals to zero (NA => zero votes for that party within the constituency)
-        for party_vote_result in results.columns[6:]:  # first 6 cols are not party votes
-            results[party_vote_result] = results[party_vote_result].fillna(0)
-        assert results.shape == (650, 144)
-
-        # Save this for convenience
-        results_full = results.copy()
-
-        ############################
-        # ADDITIONAL TRANSFORMATIONS
-        ############################
-
-        # Filter to metadata cols + parties of interest (from ~139 parties to ~11).
-        parties_lookup = {
-            "Con": "con",
-            "Lab": "lab",
-            "LD": "ld",
-            "UKIP": "ukip",
-            "Grn": "grn",
-            # Northern Ireland
-            "DUP": "dup",
-            "SF": "sf",
-            "SDLP": "sdlp",
-            # Scotland
-            "SNP": "snp",
-            # Wales
-            "PC": "pc",
-            # Other
-            "Other": "other",
-        }
-        other_parties = list(set(results.columns) - set(results.columns[:6]) - set(parties_lookup.keys()))
-
-        results["Other"] = results.loc[:, other_parties].sum(axis=1)
-        results = results.loc[:, list(results.columns[:6]) + list(parties_lookup.keys())]
-
-        # Rename parties (if in parties_lookup, else ignore)
-        # TODO: Cleaner with .rename()
-        results.columns = [parties_lookup[x] if x in parties_lookup else x for x in results.columns]
-
-        # Calculate constituency level vote share % (pc = percent)
-        for party in parties_lookup.values():
-            results[party + "_pc"] = results[party] / results["Votes"]
-
-        # Create PANO -> geo lookup
-        results["geo"] = results.Region.map(
-            {
-                "East Midlands": "England_not_london",
-                "Eastern": "England_not_london",
-                "London": "London",
-                "North East": "England_not_london",
-                "North West": "England_not_london",
-                "Northern Ireland": "NI",
-                "Scotland": "Scotland",
-                "South East": "England_not_london",
-                "South West": "England_not_london",
-                "Wales": "Wales",
-                "West Midlands": "England_not_london",
-                "Yorkshire and the Humber": "England_not_london",
-            }
+        parties = ["Con", "LD", "Lab", "UKIP", "Grn", "SNP", "PC", "DUP", "SF", "SDLP", "UUP", "APNI", "Other"]
+        results = pd.read_excel(
+            self.directory / "raw" / filename, sheet_name="2010", skiprows=4, header=None, skipfooter=19
         )
-        assert results.loc[237.0, "geo"] == "London"
 
-        # Who won?
-        def winner(row):
-            """Return winning party for given row of constituency outcomes."""
-            # Need to remove Other as this represents multiple parties so (usually) not the actual FPTP winner.
-            all_parties = set(results_full.columns[6:]) - set(["Other"])
-            winning_party = row[all_parties].sort_values(ascending=False).index[0]
-            if winning_party in parties_lookup.keys():
-                winning_party = parties_lookup[winning_party]
-            elif winning_party == "Speaker":
-                winning_party = "other"
-            return winning_party
+        # Specify columns (spread across multiple rows in Excel)
+        cols = ["", "id", "Constituency", "County", "Country/Region", "Country", "Electorate", ""]
+        for party in parties:
+            cols += [f"{party}_Votes", f"{party}_Voteshare", ""]
+        cols += ["Total votes", "Turnout"]
+        results.columns = cols
 
-        results["winner"] = results_full.apply(winner, axis=1)
-        assert results.winner.value_counts()[0] == 306  # Check Conservatives won 306 seats in 2010
+        # Some basic data quality checks
+        for party in parties:
+            assert (results[f"{party}_Voteshare"] - results[f"{party}_Votes"] / results["Total votes"]).sum() == 0
+        assert (
+            results[[f"{party}_Votes" for party in parties]].fillna(0.0).sum(axis=1) == results["Total votes"]
+        ).all()
+        assert ((results["Total votes"] / results["Electorate"]) == results["Turnout"]).all()
 
-        # EXPORT
+        # Drop blank columns plus those that can be calculated
+        cols_to_drop = [""] + [c for c in cols if "Voteshare" in c] + ["Total votes", "Turnout"]
+        results = results.drop(columns=cols_to_drop)
+
+        # Sanitise column names
+        results.columns = [utils.sanitise(c) for c in results.columns]
+        results = results.rename(columns={"id": "ons_id", "country/region": "region"})
+        results.columns = [c.replace("_votes", "") for c in results.columns]
+
+        # Reshape to long
+        results_long = pd.melt(
+            results,
+            id_vars=["ons_id", "constituency", "county", "region", "country", "electorate"],
+            var_name="party",
+            value_name="votes",
+        )
+        assert results.shape == (650, 19)
+        assert results_long.shape == (650 * len(parties), 19 - len(parties) + 2)
+
+        # Sort by (ons_id, party)
+        results_long["party"] = pd.Categorical(
+            results_long.party, categories=pd.Series(parties).apply(utils.sanitise), ordered=True
+        )
+        results_long = results_long.sort_values(["ons_id", "party"]).reset_index(drop=True)
+
+        # Re-add total_votes & voteshare
+        results_long["total_votes"] = results_long.ons_id.map(results_long.groupby("ons_id").votes.sum().astype(int))
+        results_long["voteshare"] = results_long["votes"] / results_long["total_votes"]
+
+        # Export
         print(f"Exporting dataset to {processed_results_location.resolve()}")
-        results.to_csv(processed_results_location, index=False)
-        print(f"Exporting dataset to {processed_results_full_location.resolve()}")
-        results_full.to_csv(processed_results_full_location, index=False)
+        results_long.to_csv(processed_results_location, index=False)
